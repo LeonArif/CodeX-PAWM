9e875f8c2f34c1004163e18181d69875d75cb4ca
Dodolhead <134390701+LeonArif@users.noreply.github.com>
2025-10-12 21:05:32 +0700

    fix: db use link

--- codex/backend/src/config/db.js
+++ codex/backend/src/config/db.js
@@ -1,13 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,      // Nama database
-  process.env.DB_USER,      // Username
-  process.env.DB_PASS,      // Password
+  process.env.DB_URL, // <--- Langsung pakai link connection string
   {
-    host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT,  // <--- port masuk ke options!
     logging: false,
   }
 );
