5478de8af55d7ce844d6aa6e80d33aa7ea0eb175
Dodolhead <134390701+LeonArif@users.noreply.github.com>
2025-10-21 11:32:46 +0700

    Update google.js

--- codex/backend/src/auth/google.js
+++ codex/backend/src/auth/google.js
@@ -5,7 +5,7 @@ const { createUser, getUserByGoogleid, getUserById } = require('../models/user')
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  callbackURL: "https://code-x-pawm-s49d.vercel.app/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     console.log("Google profile:", profile); // LOG PROFILE
